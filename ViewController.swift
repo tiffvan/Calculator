5f9788112fbe5ff016cdeb5a50f3575747c0eb82
Tiffany <>
2019-09-28 13:22:26 +0200

    Initial commit

---
+++ ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Tiffany on 2019/09/28.
+//  Copyright © 2019 Tiffany van der Merwe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
